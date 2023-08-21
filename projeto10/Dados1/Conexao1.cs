667a89cd409a4366c2f2203812cb497caff3a28d
Cadu 84 <developercadu84@gmail.com>
2023-08-21 16:38:38 -0300

    Adicionando Banco de Dados e Conexao

--- projeto10/Dados1/Conexao1.cs
+++ projeto10/Dados1/Conexao1.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,33 @@ namespace projeto10.Dados1
 {
     public class Conexao1
     {
+        MySqlConnection cn = new MySqlConnection("Server=localhost; DataBase=BD; User=root 3306;pwd=141184");
+        public static string msg;
+
+        public MySqlConnection MyConectarBD() //Metodo: MyConectarBD()
+        {
+            try
+            {
+                cn.Open();
+            }
+            catch (Exception erro)
+            {
+                msg = "Ocorreu um erro ao se conectar" + erro.Message;
+            }
+            return cn;
+        }
+
+        public MySqlConnection MyDesConectarBD() //Metodo: MyDesConectarBD()
+        {
+            try
+            {
+                cn.Close();
+            }
+            catch (Exception erro)
+            {
+                msg = "Ocorreu um erro ao se conectar" + erro.Message;
+            }
+            return cn;
+        }
     }
 }
